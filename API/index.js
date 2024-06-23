3c42c1b3a36dc8596579116b51652bc4b687359e
jusu <erikvixx@gmail.com>
2024-06-23 10:26:30 -0300

    feat: shadcn included

--- API/index.js
+++ API/index.js
@@ -9,29 +9,29 @@ const emitClientCount = () => {
   io.emit("clientsCount", clientsCount);
 };
 
+const chatHistory = [];
+
 io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
 
-  // Adiciona o usuário a uma sala chamada "chatRoom"
   socket.join("chatRoom");
+  
+  socket.emit("chatHistory", chatHistory);
 
-  // Notifica todos na sala que um novo usuário entrou
   io.to("chatRoom").emit("message", `${socket.id} has joined the chat`);
 
-  // Atualiza e emite o número de clientes conectados
   emitClientCount();
 
-  // Recebe mensagens dos clientes e retransmite para todos na sala
   socket.on("message", (message) => {
+    chatHistory.push({ id: socket.id, message });
     io.to("chatRoom").emit("message", `${socket.id}: ${message}`);
+    console.log("All messages", chatHistory);
   });
 
-  // Notifica todos na sala quando um usuário se desconecta
   socket.on("disconnect", () => {
     io.to("chatRoom").emit("message", `${socket.id} has left the chat`);
     console.log("user disconnected", socket.id);
 
-    // Atualiza e emite o número de clientes conectados
     emitClientCount();
   });
 });
